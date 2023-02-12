b26a1a2dc0aec6451ce2c984dfabd6bb66cb1ce9
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2023-02-12 17:32:26 +0700

    e2e update

--- e2e/Liking_Resto.spec.js
+++ e2e/Liking_Resto.spec.js
@@ -6,6 +6,5 @@ Before(({ I }) => {
 
   Scenario('showing empty liked resto', ({ I }) => {
     I.seeElement('#query');
-    I.waitForElement;
     I.see('Tidak ada resto untuk ditampilkan', '.resto-item__not__found');
   });
